97a126ed9bafae7e27dfafb6fed25d2981f1a2a1
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-08-27 18:05:11 +0300

    remove repeated code

--- backend.py
+++ backend.py
@@ -21,29 +21,25 @@ def connect(database_name="news"):
 
 def getTopArticles():
     """Returns the list of top aricles"""
-    db, cursor = connect()
-    cursor.execute(
-        "SELECT title, views FROM topArticles")
-    articles = cursor.fetchall()
-    db.close()
-    return articles
+    return execute_query("SELECT title, views FROM topArticles")
 
 
 def getTopAuthors():
     """Returns the list of top authors"""
-    db, cursor = connect()
-    cursor.execute(
-        "SELECT author, views FROM topAuthors")
-    authors = cursor.fetchall()
-    db.close()
-    return authors
+    return execute_query("SELECT author, views FROM topAuthors")
 
 
 def getBadDays():
     """Returns the list of days with a large percent of request failures"""
-    db, cursor = connect()
-    cursor.execute(
-        "SELECT date, errpercent FROM badDays")
-    days = cursor.fetchall()
-    db.close()
-    return days
+    return execute_query("SELECT date, errpercent FROM badDays")
+
+
+def execute_query(query):
+    try:
+        db, cursor = connect()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        db.close()
+        return result
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
