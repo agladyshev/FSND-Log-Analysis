6e2096fbf2dfe2253dd1490da799b56f8b75bf24
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-08-27 18:37:36 +0300

    update db views for speed, fix rounding bug

--- views.sql
+++ views.sql
@@ -1,34 +1,39 @@
 \c news
 
+CREATE VIEW viewsByPath AS
+	SELECT path, count(*) AS views
+	FROM log
+	GROUP BY path;
+
 
 CREATE VIEW topArticles AS
-	SELECT a.title as title, count(l.id) as views
-	FROM articles as a LEFT JOIN log as l
-	ON a.slug = split_part(l.path, '/', 3)
-	GROUP BY title 
+	SELECT a.title as title, v.views
+	FROM articles as a 
+	LEFT JOIN viewsByPath as v
+	ON '/article/' || a.slug = v.path
 	ORDER BY views DESC
 	LIMIT 3;
 
 
 CREATE VIEW topAuthors AS
-	SELECT authors.name as author, count(log.id) as views
+	SELECT authors.name as author, v.views
 	FROM authors
 	RIGHT JOIN articles
 	ON authors.id = articles.author
-	LEFT JOIN log
-	ON articles.slug = split_part(log.path, '/', 3)
-	GROUP BY authors.name
+	LEFT JOIN viewsByPath as v
+	ON '/article/' || articles.slug = v.path
 	ORDER BY views DESC
 	LIMIT 3;
 
 
 CREATE VIEW badDays AS
-	SELECT date, errpercent
+	SELECT date, to_char(float8(percent), 'FM999999999.00') as errpercent
 	FROM
 	(
-		SELECT time::date as date, ( 100 * count(
-			CASE WHEN status != '200 OK' THEN 1 END) / count(id) ) as errpercent
+		SELECT time::date as date, 
+		(CAST(100 * count(CASE WHEN status != '200 OK' THEN 1 END) AS float)
+		/ CAST(count(id) AS float)) AS percent
 		FROM log
 		GROUP BY time::date
 	) data
-	WHERE errpercent >= 1;
+	WHERE percent >= 1;
